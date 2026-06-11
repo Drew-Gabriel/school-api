208deef2232042accaef3057e4ac9d06cd8ee0ab
Drew Gabriel <gabjacz@gmail.com>
2026-06-11 18:42:56 -0500

    Added error handling

--- controllers/courses.js
+++ controllers/courses.js
@@ -2,39 +2,70 @@ const { getDb } = require("../db/connect");
 const { ObjectId } = require("mongodb");
 
 const getAll = async (req, res) => {
-  const db = getDb();
-  res.json(await db.collection("courses").find().toArray());
+  try {
+    const db = getDb();
+    const data = await db.collection("courses").find().toArray();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 const getSingle = async (req, res) => {
-  const db = getDb();
-  const data = await db.collection("courses").findOne({
-    _id: new ObjectId(req.params.id)
-  });
-  res.json(data);
+  try {
+    const db = getDb();
+
+    const data = await db.collection("courses").findOne({
+      _id: new ObjectId(req.params.id)
+    });
+
+    if (!data) {
+      return res.status(404).json({ message: "Course not found" });
+    }
+
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 const createCourse = async (req, res) => {
-  const db = getDb();
-  const result = await db.collection("courses").insertOne(req.body);
-  res.status(201).json(result);
+  try {
+    const db = getDb();
+    const result = await db.collection("courses").insertOne(req.body);
+    res.status(201).json(result);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 const updateCourse = async (req, res) => {
-  const db = getDb();
-  const result = await db.collection("courses").updateOne(
-    { _id: new ObjectId(req.params.id) },
-    { $set: req.body }
-  );
-  res.json(result);
+  try {
+    const db = getDb();
+
+    const result = await db.collection("courses").updateOne(
+      { _id: new ObjectId(req.params.id) },
+      { $set: req.body }
+    );
+
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 const deleteCourse = async (req, res) => {
-  const db = getDb();
-  const result = await db.collection("courses").deleteOne({
-    _id: new ObjectId(req.params.id)
-  });
-  res.json(result);
+  try {
+    const db = getDb();
+
+    const result = await db.collection("courses").deleteOne({
+      _id: new ObjectId(req.params.id)
+    });
+
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 module.exports = {
--- controllers/students.js
+++ controllers/students.js
@@ -2,39 +2,70 @@ const { getDb } = require("../db/connect");
 const { ObjectId } = require("mongodb");
 
 const getAll = async (req, res) => {
-  const db = getDb();
-  res.json(await db.collection("students").find().toArray());
+  try {
+    const db = getDb();
+    const data = await db.collection("students").find().toArray();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 const getSingle = async (req, res) => {
-  const db = getDb();
-  const data = await db.collection("students").findOne({
-    _id: new ObjectId(req.params.id)
-  });
-  res.json(data);
+  try {
+    const db = getDb();
+
+    const data = await db.collection("students").findOne({
+      _id: new ObjectId(req.params.id)
+    });
+
+    if (!data) {
+      return res.status(404).json({ message: "Student not found" });
+    }
+
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 const createStudent = async (req, res) => {
-  const db = getDb();
-  const result = await db.collection("students").insertOne(req.body);
-  res.status(201).json(result);
+  try {
+    const db = getDb();
+    const result = await db.collection("students").insertOne(req.body);
+    res.status(201).json(result);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 const updateStudent = async (req, res) => {
-  const db = getDb();
-  const result = await db.collection("students").updateOne(
-    { _id: new ObjectId(req.params.id) },
-    { $set: req.body }
-  );
-  res.json(result);
+  try {
+    const db = getDb();
+
+    const result = await db.collection("students").updateOne(
+      { _id: new ObjectId(req.params.id) },
+      { $set: req.body }
+    );
+
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 const deleteStudent = async (req, res) => {
-  const db = getDb();
-  const result = await db.collection("students").deleteOne({
-    _id: new ObjectId(req.params.id)
-  });
-  res.json(result);
+  try {
+    const db = getDb();
+
+    const result = await db.collection("students").deleteOne({
+      _id: new ObjectId(req.params.id)
+    });
+
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 module.exports = {
